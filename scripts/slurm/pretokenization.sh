417973e3bbaeaaca59949db594a7cc047b0f8578
lavaman131 <lavaalex3@gmail.com>
2025-03-23 19:40:56 -0400

    changed pretokenization script

--- scripts/slurm/pretokenization.sh
+++ scripts/slurm/pretokenization.sh
@@ -2,8 +2,8 @@
 
 #$ -P dl4ds
 #$ -l h_rt=24:00:00
-#$ -l gpus=4
-#$ -l gpu_memory=48G
+#$ -l gpus=2
+#$ -l gpu_memory=80G
 #$ -N pretokenization
 #$ -pe omp 8
 #$ -j y # Merge the error and output streams into a single file
@@ -34,14 +34,14 @@ NODE_RANK=0 # rank of the node
 
 torchrun \
     --nnodes 1 \
-    --nproc_per_node 4 \
+    --nproc_per_node 2 \
     --node_rank $NODE_RANK \
     --rdzv-id $RDZV_ID \
     --rdzv-backend $RDZV_BACKEND \
     --rdzv-endpoint $RDZV_ENDPOINT \
     ./scripts/train/pretokenization.py \
     --img_size 256 \
-    --batch_size 20 \
+    --batch_size 40 \
     --ten_crop \
     --data_path $DATA_DIR/$SUFFIX \
     --cached_path $SAVE_DIR
