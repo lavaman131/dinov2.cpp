d05a8277ae667a9413390c04ed86e45f2ed3377b
lavaman131 <lavaalex3@gmail.com>
2025-04-13 14:18:40 -0400

    modified benchmark code

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,33 +1,60 @@
 import time
+from typing import Tuple, Final, Sequence
 import torch
-import torchvision.transforms as transforms
+from torchvision.transforms import v2
 from memory_profiler import memory_usage
 from PIL import Image
 from threadpoolctl import threadpool_limits
+from transformers import AutoModel, AutoConfig
+import torch.nn.functional as F
+
+IMAGENET_DEFAULT_MEAN: Final[Sequence[float]] = (0.485, 0.456, 0.406)
+IMAGENET_DEFAULT_STD: Final[Sequence[float]] = (0.229, 0.224, 0.225)
 
 
-def process_and_predict(image_path, model_path):
-    model = timm.create_model(model_path, pretrained=True)
-    preprocess = transforms.Compose(
-        [
-            transforms.Resize((224, 224)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-        ]
-    )
+def make_normalize_transform(
+        mean: Sequence[float] = IMAGENET_DEFAULT_MEAN,
+        std: Sequence[float] = IMAGENET_DEFAULT_STD,
+) -> v2.Normalize:
+    return v2.Normalize(mean=mean, std=std)
+
+
+# This matches (roughly) torchvision's preset for classification evaluation:
+#   https://github.com/pytorch/vision/blob/main/references/classification/presets.py#L47-L69
+def make_classification_eval_transform(
+        *,
+        resize_size: int = 256,
+        interpolation=v2.InterpolationMode.BICUBIC,
+        crop_size: int = 224,
+        mean: Sequence[float] = IMAGENET_DEFAULT_MEAN,
+        std: Sequence[float] = IMAGENET_DEFAULT_STD,
+) -> v2.Compose:
+    transforms_list = [
+        v2.Resize(resize_size, interpolation=interpolation),
+        v2.CenterCrop(crop_size),
+        v2.ToImage(),
+        v2.ToDtype(torch.float32, scale=True),
+        make_normalize_transform(mean=mean, std=std),
+    ]
+    return v2.Compose(transforms_list)
+
+
+def process_and_predict(image_path: str, model_path: str) -> torch.Tensor:
+    model = AutoModel.from_pretrained(model_path)
+    preprocess = make_classification_eval_transform()
 
     image = Image.open(image_path).convert("RGB")
     image = preprocess(image)
-    image = image.unsqueeze(0)
+    image = image.unsqueeze_(0)
 
-    with torch.no_grad():
+    with torch.inference_mode():
         outputs = model(image)
-        probabilities = torch.nn.functional.softmax(outputs, dim=1)
+        probabilities = F.softmax(outputs, dim=-1)
 
     return probabilities
 
 
-def benchmark_model(image_path, model_name, N=10):
+def benchmark_model(image_path: str, model_name: str, N: int = 10) -> Tuple[float, float]:
     times = []
     peak_memory_usages = []
 
@@ -53,18 +80,17 @@ def benchmark_model(image_path, model_name, N=10):
     return avg_time, max_peak_memory
 
 
-# model variants
-model_variants = {
-    "tiny": "vit_tiny_patch16_224.augreg_in21k_ft_in1k",
-    "small": "vit_small_patch16_224.augreg_in21k_ft_in1k",
-    "base": "vit_base_patch16_224.augreg_in21k_ft_in1k",
-    "large": "vit_large_patch16_224.augreg_in21k_ft_in1k",
-}
+def main() -> None:
+    # model variants
+    model_variants = {
+        "small": "facebook/dinov2-small-imagenet1k-1-layer",
+        "base": "facebook/dinov2-base-imagenet1k-1-layer",
+        "large": "facebook/dinov2-large-imagenet1k-1-layer",
+        "giant": "facebook/dinov2-giant-imagenet1k-1-layer",
+    }
 
-# an image
-image_path = "./assets/tench.jpg"
-
-if __name__ == "__main__":
+    # an image
+    image_path = "./assets/tench.jpg"
     with threadpool_limits(limits=4):
         print("| Model | Speed (ms)   |   Mem (MB)       |")
         print("|-------|--------------|------------------|")
@@ -72,3 +98,7 @@ if __name__ == "__main__":
         for name, model_name in model_variants.items():
             avg_time, peak_memory = benchmark_model(image_path, model_name)
             print(f"| {name} | {avg_time:.0f} | {peak_memory:.0f} |")
+
+
+if __name__ == "__main__":
+    main()
