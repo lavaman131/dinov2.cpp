e16598399668e0f53ea831224ab4246d1b9fa041
lavaman131 <lavaalex3@gmail.com>
2025-04-21 11:32:27 -0400

    updated benchmark script

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,20 +1,25 @@
 import time
-from typing import Tuple, Final, Sequence
+from typing import Tuple, Final, Sequence, Dict, Any, Union
 import torch
 from torchvision.transforms import v2
 from memory_profiler import memory_usage
 from PIL import Image
 from threadpoolctl import threadpool_limits
-from transformers import Dinov2ForImageClassification, AutoModel, AutoConfig
+from transformers import AutoImageProcessor, AutoModelForImageClassification
 import torch.nn.functional as F
+import torch.nn as nn
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("transformers").setLevel(logging.ERROR)
 
 IMAGENET_DEFAULT_MEAN: Final[Sequence[float]] = (0.485, 0.456, 0.406)
 IMAGENET_DEFAULT_STD: Final[Sequence[float]] = (0.229, 0.224, 0.225)
 
 
 def make_normalize_transform(
-    mean: Sequence[float] = IMAGENET_DEFAULT_MEAN,
-    std: Sequence[float] = IMAGENET_DEFAULT_STD,
+        mean: Sequence[float] = IMAGENET_DEFAULT_MEAN,
+        std: Sequence[float] = IMAGENET_DEFAULT_STD,
 ) -> v2.Normalize:
     return v2.Normalize(mean=mean, std=std)
 
@@ -22,12 +27,12 @@ def make_normalize_transform(
 # This matches (roughly) torchvision's preset for classification evaluation:
 #   https://github.com/pytorch/vision/blob/main/references/classification/presets.py#L47-L69
 def make_classification_eval_transform(
-    *,
-    resize_size: int = 256,
-    interpolation=v2.InterpolationMode.BICUBIC,
-    crop_size: int = 224,
-    mean: Sequence[float] = IMAGENET_DEFAULT_MEAN,
-    std: Sequence[float] = IMAGENET_DEFAULT_STD,
+        *,
+        resize_size: int = 256,
+        interpolation=v2.InterpolationMode.BICUBIC,
+        crop_size: int = 224,
+        mean: Sequence[float] = IMAGENET_DEFAULT_MEAN,
+        std: Sequence[float] = IMAGENET_DEFAULT_STD,
 ) -> v2.Compose:
     transforms_list = [
         v2.Resize(resize_size, interpolation=interpolation),
@@ -39,46 +44,43 @@ def make_classification_eval_transform(
     return v2.Compose(transforms_list)
 
 
-def process_and_predict(image_path: str, model_path: str) -> torch.Tensor:
-    device = "cpu"
-    model = Dinov2ForImageClassification.from_pretrained(model_path, device_map=device)
-    preprocess = make_classification_eval_transform()
-
-    image = Image.open(image_path).convert("RGB")
-    image = preprocess(image).to(device, non_blocking=True).unsqueeze_(0)
-
-    with torch.inference_mode():
-        logits = model(image).logits
-        probabilities = F.softmax(logits, dim=-1)
-
+@torch.inference_mode
+def predict(inputs: Dict[str, Any], model: nn.Module) -> torch.Tensor:
+    logits = model(**inputs).logits
+    probabilities = F.softmax(logits, dim=-1)
     return probabilities
 
 
 def benchmark_model(
-    image_path: str, model_name: str, N: int = 10
+        image_path: str, model_name: str, n: int, device: str
 ) -> Tuple[float, float]:
     times = []
     peak_memory_usages = []
 
-    for _ in range(N):
-        start_time = time.time()
+    model = AutoModelForImageClassification.from_pretrained(model_name)
+    preprocess = make_classification_eval_transform()
+    with Image.open(image_path).convert("RGB") as image:
+        inputs = dict(pixel_values=preprocess(image).to(device, non_blocking=True).unsqueeze_(0))
 
+    for _ in range(n):
         # Measure peak memory usage
         peak_memory_usage = memory_usage(
-            (process_and_predict, (image_path, model_name)),
+            (predict, (inputs, model)),
             interval=0.01,
             max_usage=True,
             include_children=True,
         )
+        peak_memory_usages.append(peak_memory_usage)
 
-        end_time = time.time()
+        start_time = time.perf_counter_ns()
+        predict(inputs, model)
+        end_time = time.perf_counter_ns()
 
         time_taken = end_time - start_time
         times.append(time_taken)
-        peak_memory_usages.append(peak_memory_usage)
 
-    avg_time = sum(times) / N * 1000  # in ms
-    max_peak_memory = sum(peak_memory_usages) / N
+    avg_time = sum(times) / n * 1e-6  # Convert to milliseconds
+    max_peak_memory = sum(peak_memory_usages) / n
     return avg_time, max_peak_memory
 
 
@@ -86,21 +88,23 @@ def main() -> None:
     # model variants
     model_variants = {
         "small": "facebook/dinov2-small-imagenet1k-1-layer",
-        "base": "facebook/dinov2-base-imagenet1k-1-layer",
-        "large": "facebook/dinov2-large-imagenet1k-1-layer",
-        "giant": "facebook/dinov2-giant-imagenet1k-1-layer",
+        # "base": "facebook/dinov2-base-imagenet1k-1-layer",
+        # "large": "facebook/dinov2-large-imagenet1k-1-layer",
+        # "giant": "facebook/dinov2-giant-imagenet1k-1-layer",
     }
 
     # an image
     image_path = "./assets/tench.jpg"
+    device = "cpu"
+    n = 10
 
-    with threadpool_limits(limits=4):
-        print("| Model | Speed (ms)   |   Mem (MB)       |")
-        print("|-------|--------------|------------------|")
+    # with threadpool_limits(limits=4):
+    print("| Model | Speed (ms)   |   Mem (MB)       |")
+    print("|-------|--------------|------------------|")
 
-        for name, model_name in model_variants.items():
-            avg_time, peak_memory = benchmark_model(image_path, model_name)
-            print(f"| {name} | {avg_time:.0f} | {peak_memory:.0f} |")
+    for name, model_name in model_variants.items():
+        avg_time, peak_memory = benchmark_model(image_path, model_name, n, device)
+        print(f"| {name} | {avg_time:.0f} | {peak_memory:.0f} |")
 
 
 if __name__ == "__main__":
